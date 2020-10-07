78a04e7549a8dc88bc0ccb5654b18bf5269b1c81
Tasha Lane <designtmg77@gmail.com>
2020-10-07 16:18:04 -0500

    initial commentRepository files ready for testing

---
+++ Tabloid/Repositories/CommentRepository.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+using Tabloid.Repositories;
+using Tabloid.Models.cs;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository 
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+        public List<Comment> GetAllPostComments(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT c.Id AS CommentId, c.Subject, c.Content, c.CreateDateTime,
+                              c.PostId, c.UserProfileId,
+                              p.Id, p.Title, p.Content, 
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              cg.[Name] AS CategoryName,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                              ut.[Name] AS UserTypeName
+                         FROM Comment c
+                              LEFT JOIN Post p ON c.PostId = p.id
+                              LEFT JOIN Category cg ON p.CategoryId = cg.id
+                              LEFT JOIN UserProfile u ON c.UserProfileId = u.id
+                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                         WHERE PostId = @id
+                         ORDER BY c.CreateDateTime DESC";
+                    cmd.Parameters.AddWithValue("@id", postId);
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(NewCommentFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+
+        public Comment GetCommentById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT c.Id AS CommentId, c.Subject, c.Content, c.CreateDateTime,
+                              c.PostId, c.UserProfileId,
+                              p.Id, p.Title, p.Content, 
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              cg.[Name] AS CategoryName,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                              ut.[Name] AS UserTypeName
+                         FROM Comment c
+                              LEFT JOIN Post p ON c.PostId = p.id
+                              LEFT JOIN Category cg ON p.CategoryId = cg.id
+                              LEFT JOIN UserProfile u ON c.UserProfileId = u.id
+                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                        WHERE c.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    Comment comment = null;
+
+                    if (reader.Read())
+                    {
+                        comment = NewCommentFromReader(reader);
+                    }
+
+                    reader.Close();
+
+                    return comment;
+                }
+            }
+        }
+
+        public void Add(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Comment (
+                             Content, CreateDateTime, PostId, UserProfileId )
+                        OUTPUT INSERTED.ID
+                        VALUES (
+                             @Content, @CreateDateTime, @PostId, @UserProfileId )";
+            
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", comment.CreateDateTime);
+                    cmd.Parameters.AddWithValue("@PostId", comment.PostId);
+                    cmd.Parameters.AddWithValue("@UserProfileId", comment.UserProfileId);
+
+                    comment.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+        public void Update(Comment comment)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            UPDATE Comment
+                            SET
+                                Content = @Content, 
+                                CreateDateTime = @createDateTime,
+                                PostId = @postId,
+                                UserProfileId = @UserProfileId
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", comment.CreateDateTime);
+                    cmd.Parameters.AddWithValue("@PostId", comment.PostId);
+                    cmd.Parameters.AddWithValue("@UserProfileId", comment.UserProfileId);
+                    cmd.Parameters.AddWithValue("@id", comment.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Comment
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private Comment NewCommentFromReader(SqlDataReader reader)
+        {
+            return new Comment()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("CommentId")),
+                Content = reader.GetString(reader.GetOrdinal("Content")),
+                CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                /*UserProfile = new UserProfile()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                    DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                    CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                    ImageLocation = DbUtils.GetNullableString(reader, "AvatarImage"),
+                    UserTypeId = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                    UserType = new UserType()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                        Name = reader.GetString(reader.GetOrdinal("UserTypeName"))
+                    }
+                },
+                PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                Post = new Post()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                    Content = reader.GetString(reader.GetOrdinal("Content")),
+                    ImageLocation = DbUtils.GetNullableString(reader, "HeaderImage"),
+                    CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                    PublishDateTime = DbUtils.GetNullableDateTime(reader, "PublishDateTime"),
+                    CategoryId = reader.GetInt32(reader.GetOrdinal("CategoryId")),
+                    Category = new Category()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("CategoryId")),
+                        Name = reader.GetString(reader.GetOrdinal("CategoryName"))
+                    }
+                }*/
+            };
+        }
+    }
+}
---
+++ Tabloid/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Tabloid.Models.cs;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        void Add(Comment comment);
+        void Delete(int id);
+        List<Comment> GetAllPostComments(int postId);
+        Comment GetCommentById(int id);
+        void Update(Comment comment);
+    }
+}
