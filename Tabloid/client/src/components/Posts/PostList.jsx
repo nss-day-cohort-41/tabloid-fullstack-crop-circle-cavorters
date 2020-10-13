857d1d956d2f45daf9f927b959a48abf310c4de7
Chad Jones <chadjones839@gmail.com>
2020-10-13 08:10:46 -0500

    saving changes

--- Tabloid/client/src/components/Posts/PostList.jsx
+++ Tabloid/client/src/components/Posts/PostList.jsx
@@ -1,7 +1,7 @@
+  
 import React, { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostContext } from "../../providers/PostProvider";
-import { Link } from "react-router-dom";
 
 export default function PostList() {
   const { posts, getAllPosts } = useContext(PostContext);
@@ -12,38 +12,9 @@ export default function PostList() {
 
   return (
     <section>
-      <div class="postCard">
-        <div className="postHeader">
-          <h1>Posts</h1>
-          <p>
-              <a class="btn btn-primary" action="Create">New Post</a>
-          </p>
-        </div>
-        <div className="post-container">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>
-                Title
-              </th>
-              <th>
-                Posted by
-              </th>
-              <th>
-                Category
-              </th>
-              <th>
-                Publish Date
-              </th>
-              <th></th>
-            </tr>
-          </thead>
-          {posts.map(p =>
-            <Post key={p.id} post={p}/>
-          )}
-        </table>
-      </div>
-      </div>
+      {posts.map(p =>
+        <Post key={p.id} post={p}/>
+      )}
     </section>
   );
 }
