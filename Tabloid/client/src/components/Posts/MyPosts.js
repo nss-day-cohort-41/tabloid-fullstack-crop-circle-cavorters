046acae86f2c1ba42434541260b0059750520a43
Zach McWhirter <zmac7811@gmail.com>
2020-10-13 17:20:09 -0500

    wip myposts

--- Tabloid/client/src/components/Posts/MyPosts.js
+++ Tabloid/client/src/components/Posts/MyPosts.js
@@ -1,58 +1,62 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostContext } from "../../providers/PostProvider";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function UserSpecificPosts() {
+    const history = useHistory();
     const { post, posts, getAllPostsByUser } = useContext(PostContext);
-    const sessionUser = JSON.parse(sessionStorage.getItem("userProfile"));
+    const [user, setUser] = useState();
+
 
     useEffect(() => {
-        getAllPostsByUser(JSON.parse(sessionStorage.getItem("userProfile")).id);
+        getAllPostsByUser();
+        const currentUser = JSON.parse(sessionStorage.getItem("userProfile"))
+        setUser(currentUser.id)
     }, []);
 
-    if (sessionUser.id === post.userProfileId) {
 
 
-        return (
-            <section>
-                <div class="postCard">
-                    <div className="postHeader">
-                        <h1>Posts</h1>
-                        <p>
-                            <Link class="btn btn-primary" to="/posts/add">New Post</Link>
-                        </p>
-                        <p>
-                            <Link class="btn btn-primary" to="/posts/myposts">My Posts</Link>
-                        </p>
-                    </div>
-                    <div className="post-container">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        Title
-                </th>
-                                    <th>
-                                        Posted by
-                </th>
-                                    <th>
-                                        Category
-                </th>
-                                    <th>
-                                        Publish Date
-                </th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            {posts.map(p =>
-                                <Post key={p.id} post={p} />
-                            )}
-                        </table>
-                    </div>
+
+    return (
+        <section>
+            <div class="postCard">
+                <div className="postHeader">
+                    <h1>Posts</h1>
+                    <p>
+                        <Link class="btn btn-primary" to="/posts/add">New Post</Link>
+                    </p>
+                    <p>
+                        <Link class="btn btn-primary" to="/posts/myposts">My Posts</Link>
+                    </p>
                 </div>
-            </section>
+                <div className="post-container">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Title
+                </th>
+                                <th>
+                                    Posted by
+                </th>
+                                <th>
+                                    Category
+                </th>
+                                <th>
+                                    Publish Date
+                </th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        {posts.map(p =>
+                            <Post key={p.id} post={p} user={user} />
+                        )}
+                    </table>
+                </div>
+            </div>
+        </section>
 
-        );
-    }
+    );
+
 }
