85bed927bda01eaf7b48854530100cb5d8af7850
Trigg Summs <stsumms@gmail.com>
2020-10-13 14:06:18 -0500

    Merged all of Lacies, testing Zachs PR

--- Tabloid/Repositories/ITagRepository.cs
+++ Tabloid/Repositories/ITagRepository.cs
@@ -6,10 +6,13 @@ namespace Tabloid.Repositories
     public interface ITagRepository
     {
         List<Tag> GetAllTags();
+        List<Tag> GetAllTagsOnAPost(int postId);
         Tag GetTagById(int id);
         void AddTag(Tag tag);
         void DeleteTag(int tagId);   
         void UpdateTag( Tag tag);
+
+        
 
     }
 }
