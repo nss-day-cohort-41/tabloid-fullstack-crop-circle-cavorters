40fb5d6875858c17d54876f6cae8db6831f540cd
Tasha Lane <designtmg77@gmail.com>
2020-10-13 13:43:15 -0500

    new merge with categories

--- Tabloid/client/src/components/Comments/CommentList.js
+++ Tabloid/client/src/components/Comments/CommentList.js
@@ -5,11 +5,11 @@ import { useParams, Link } from "react-router-dom";
 import { Button, } from "reactstrap";
 import { PostContext } from "../../providers/PostProvider";
 import Comment from "./Comment";
-
+import { useHistory } from "react-router-dom";
 
 export default function CommentList() {
     const { comments, getAllCommentsByPostId } = useContext(CommentContext);
-    //const { post, getPost } = useContext(PostContext)
+    const { post, getPost } = useContext(PostContext)
     const { id } = useParams();
 
     useEffect(() => {
@@ -26,7 +26,18 @@ export default function CommentList() {
                     <h1>Comments</h1>
                     <Link to={`comments/add`}><Button color="primary">Add New Comment</Button></Link>
                 </div>
-                {/* { comments.map((comment) => {
+
+                <h4>Subject</h4>
+                <p>{comments && comments.subject}</p>
+                <h6>Comment</h6>
+                <p>{comments && comments.content}</p>
+                <Button onClick={() => history.push(`/comments/${comments.postId}`)}>
+                    Back To Posts
+            </Button>
+            </section>
+
+
+            {/* { comments.map((comment) => {
                     return (
                         <Comment key={comment.id} comment={comment} />
                     )
@@ -34,7 +45,7 @@ export default function CommentList() {
 
                 )} */}
 
-            </section>
+
         </>
     );
 };
