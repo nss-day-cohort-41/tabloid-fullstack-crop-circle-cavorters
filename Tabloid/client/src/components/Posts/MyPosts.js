e70d823e3fde53a44106854a18d58e2cf0c8fc63
Chad Jones <chadjones839@gmail.com>
2020-10-21 08:03:56 -0500

    styles my posts

--- Tabloid/client/src/components/Posts/MyPosts.js
+++ Tabloid/client/src/components/Posts/MyPosts.js
@@ -26,41 +26,42 @@ export default function UserSpecificPosts() {
         <section>
             <div class="postCard">
                 <div className="postHeader">
-                    {/* adding the current sessionUser's display name to the H1 */}
-                    <h1>{(JSON.parse(userProfile).displayName)}'s Posts</h1>
-                    <p>
-                        {/* <Link class="btn btn-secondary" to="/posts/add">New Post</Link> */}
-                        {/* </p>
-                    <p> */}
-                        <Link class="approvedPosts" to={`/posts/`}>All Posts</Link>
-                    </p>
+                    <div className="postHeaderDetails">
+                        <div>
+                            <h1>{(JSON.parse(userProfile).displayName)}'s Posts</h1>
+                        </div>
+                        <div>
+                            <p>
+                                <a class="approvedPosts" href="/posts">All Posts</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
+            
                 <div className="post-container">
-                    <table class="table table-striped">
-                        <thead>
+                    <table className="postTable">
+                        <thead className="postTableHeader">
                             <tr>
-                                <th>
-                                    Title
-                </th>
-                                <th>
-                                    Posted by
-                </th>
-                                <th>
-                                    Category
-                </th>
-                                <th>
-                                    Publish Date
-                </th>
-                                <th></th>
+                            <th className="postTitle-header">
+                                Title
+                            </th>
+                            <th className="postUserName-header">
+                                Author
+                            </th>
+                            <th className="postCategory-header">
+                                Category
+                            </th>
+                            <th className="postDate-header">
+                                Publish Date
+                            </th>
+                            <th></th>
                             </tr>
                         </thead>
                         {posts.map(p =>
-                            <Post key={p.id} post={p} user={user} />
+                            <Post key={p.id} post={p} />
                         )}
-
                     </table>
                 </div>
-
             </div>
         </section>
 
--- Tabloid/client/src/components/Posts/PostList.jsx
+++ Tabloid/client/src/components/Posts/PostList.jsx
@@ -18,39 +18,6 @@ export default function PostList() {
     getAllPosts();
   }, []);
 
-  // return (
-  //   <section>
-  //     <div class="postCard">
-  //       <div className="postHeader">
-  //         <h1>All Posts</h1>
-  //         <p>
-  //           <Link class="btn btn-secondary" to="/posts/add">New Post</Link>
-  //           {/* </p>
-  //         <p> */}
-  //           <Link class="btn btn-success" to={`/posts/myposts/${userProfile.id}`}>My Posts</Link>
-  //         </p>
-  //       </div>
-  //       <div className="post-container">
-
-  //         <table class="table table-striped">
-  //           <thead>
-  //             <tr>
-  //               <th>
-  //                 Title
-  //             </th>
-  //               <th>
-  //                 Posted by
-  //             </th>
-  //               <th>
-  //                 Category
-  //             </th>
-  //               <th>
-  //                 Publish Date
-  //             </th>
-
-  //             </tr>
-  //           </thead>
-
   if (sessionUser.userTypeId === 1) {
     return (
       <section>
